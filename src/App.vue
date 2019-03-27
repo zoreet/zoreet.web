04d95b0c5c2131a6c777aa4474e415d166c6812f
Zoreet <zoreetdotcom@gmail.com>
2019-03-27 16:45:16 +0100

    vuex exploration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,144 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-cloak v-touch:swipe="swipeHandler">
+    <div class="window">
+      <div>day: {{day.id}} {{ day.isToday}}</div>
+      <!-- <Component msg="Yes" id="editor"></Component> -->
+      <button @click.prevent="gotoPrevDay">Prev</button>
+      <button @click.prevent="gotoToday">Today</button>
+      <button @click.prevent="gotoNextDay">Next</button>
+      <div>
+        <button @click.prevent="editTask">Change to vlad</button>
+      </div>
+
+      <ul>
+        <li v-for="task in tasks">{{task.title}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Component from './components/Component.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    // Component,
+  },
+  computed: {
+    day() {
+      return this.$store.getters.day
+    },
+    today() {
+      return this.$store.state.today
+    },
+    tasks() {
+      return this.$store.state.tasks
+    },
+  },
+  mounted() {
+    this.$store.subscribe((mutation, state) => {
+      if (mutation.type == 'changeDay') {
+        this.loadTasks()
+      } else if (mutation.type == 'changeTask') {
+        this.saveTasks()
+      }
+    })
+  },
+  methods: {
+    // Days
+    gotoPrevDay() {
+      this.$store.commit('changeDay', { step: -1 })
+    },
+    gotoNextDay() {
+      this.$store.commit('changeDay', { step: 1 })
+    },
+    gotoToday() {
+      this.$store.commit('changeDay', { today: true })
+    },
+
+    //Tasks
+    editTask() {
+      this.$store.commit('editTask', {
+        task: this.$store.state.tasks[0],
+        title: 'Vlad',
+      })
+    },
+    loadTasks() {
+      console.log('Loading tasks')
+    },
+    saveTasks() {
+      console.log('Saving tasks')
+    },
+
+    //UI
+    swipeHandler(direction) {
+      if (direction == 'left') {
+        this.gotoPrevDay()
+      } else if (direction == 'right') {
+        this.gotoNextDay()
+      }
+    },
+  },
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+[v-cloak] {
+  display: none !important;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Helvetica,
+    Arial, sans-serif;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--front);
+  display: flex;
+  filter: saturate(0);
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+  position: fixed;
+  width: 100%;
+}
+
+.window {
+  flex: 1 1 auto;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+* {
+  box-sizing: border-box;
+  -webkit-overflow-scrolling: touch;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+::selection {
+  background: #2be4a4;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  padding: 0;
+}
+
+/* Colour Palette */
+html {
+  --front: #202020;
+  --back: #fff;
+  --back--dark: #f2f2f2;
+  --accent: #2be4a4;
+  --accent--dark: #16ac78;
+  --accent--text: #16ac78;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,63 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js'
+import Vuex from 'vuex'
 import App from './App.vue'
-import './registerServiceWorker'
+import moment from 'moment'
+import Vue2TouchEvents from 'vue2-touch-events'
+
+// import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(Vue2TouchEvents, {
+  swipeTolerance: 100,
+})
+
+const store = new Vuex.Store({
+  state: {
+    today: moment(),
+    currentDay: moment(),
+    tasks: [
+      {
+        id: 1,
+        title: 'title',
+        done: false,
+      },
+    ],
+  },
+  getters: {
+    day(state) {
+      let id = state.currentDay.format('YYYYMMDD')
+      let isToday = id === state.today.format('YYYYMMDD')
+      return {
+        id,
+        isToday,
+      }
+    },
+  },
+  mutations: {
+    changeDay(state, payload) {
+      let currentDay = state.currentDay
+      state.currentDay = ''
+      if (payload.step) {
+        //-1 or 1
+        state.currentDay = currentDay.add(payload.step, 'days')
+      } else if (payload.today) {
+        state.currentDay = moment(state.today.format('YYYYMMDD'))
+      }
+    },
+    editTask(state, payload) {
+      let task = payload.task
+      let index = state.tasks.indexOf(task)
+      task.title = payload.title
+
+      state.tasks.splice(index, task)
+    },
+  },
+})
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store,
+  template: '<App/>',
+  components: { App },
+})
