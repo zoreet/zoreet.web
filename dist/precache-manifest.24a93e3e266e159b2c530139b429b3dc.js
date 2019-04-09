2f20aa10c4ca809fceb9eb6decee785d4ae112ff
Zoreet <zoreetdotcom@gmail.com>
2019-04-10 00:47:06 +0300

    publish v3

---
+++ dist/precache-manifest.24a93e3e266e159b2c530139b429b3dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8afdacc33d75844aef44",
+    "url": "/app/css/app.3ca2b8ec.css"
+  },
+  {
+    "revision": "8afdacc33d75844aef44",
+    "url": "/app/js/app.a0cdf02d.js"
+  },
+  {
+    "revision": "84fd2f16ab58e608efe9",
+    "url": "/app/js/chunk-vendors.79ea69e4.js"
+  },
+  {
+    "revision": "d38d8bfbcc8bfec8d7e5de03c2d34d80",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app/robots.txt"
+  }
+];
