cc12fe6e53c602892ec1e50bb4529a9464552a7d
zoreet <zoreetdotcom@gmail.com>
2021-06-23 16:57:33 +0200

    config firebase to deploy from the firebase branch

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { firestorePlugin } from "vuefire";
+
+Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false;
 
