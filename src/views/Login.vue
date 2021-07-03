7325ea0a843649243f0c71024ddbfd6116575999
zoreet <zoreetdotcom@gmail.com>
2021-07-03 11:34:16 +0200

    login is done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,30 +1,119 @@
 <template>
   <div class="login">
     <navbar />
-    <hero>
-      <template slot="title">An easier way to login?</template>
-      <template slot="content">
-        <p>
-          Get a magic link sent to your email that will sign you in instantly
-        </p>
-        <input
-          id="email"
-          type="text"
-          class="input"
-          placeholder="your@email.com"
-        />
-        <button class="button --full">Send</button>
-      </template>
-    </hero>
+    <div v-if="showLoginScreen">
+      <hero>
+        <template slot="title">An easier way to login?</template>
+        <template slot="content">
+          <p class="mb-12">
+            Get a magic link sent to your email that will sign you in instantly
+          </p>
+          <form @submit="login">
+            <input
+              id="email"
+              v-model="email"
+              type="email"
+              class="input-text w-96 mr-1"
+              placeholder="your@email.com"
+              autofocus
+            />
+            <button class="btn btn--primary" @click="login">Send</button>
+            <div v-if="showInvalidMailMessage">
+              <p
+                class="
+                  mt-4
+                  text-base text-white-500
+                  bg-destroy-600
+                  border border-destroy-500
+                  inline-block
+                  px-4
+                  py-2
+                  rounded-md
+                "
+              >
+                The email address you provided is not valid.
+              </p>
+            </div>
+          </form>
+        </template>
+      </hero>
+    </div>
+    <div v-else>
+      <hero>
+        <template slot="title">Check your inbox</template>
+        <template slot="content">
+          <p class="mb-12">
+            We sent a magic link to <strong>{{ email }}</strong
+            >. Click it and it will sign you in instantly.
+          </p>
+        </template>
+      </hero>
+    </div>
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue";
 import Navbar from "../components/Navbar.vue";
+import firebase from "firebase/app";
+
+const screens = {
+  login: "login",
+  sent: "sent",
+};
+
 export default {
   components: { Hero, Navbar },
   name: "Login",
+  data() {
+    return {
+      email: "",
+      formIsValid: true,
+      activeScreen: screens.login,
+    };
+  },
+  computed: {
+    showLoginScreen() {
+      return this.activeScreen === screens.login;
+    },
+    showInvalidMailMessage() {
+      return !this.emailIsValid && !this.formIsValid;
+    },
+    emailIsValid() {
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(this.email).toLowerCase());
+    },
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+      this.formIsValid = true;
+      if (!this.email || !this.emailIsValid) {
+        this.formIsValid = false;
+        return;
+      }
+
+      var actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be in the authorized domains list in the Firebase Console.
+        url: window.location.href,
+        // This must be true.
+        handleCodeInApp: true,
+      };
+
+      firebase
+        .auth()
+        .sendSignInLinkToEmail(this.email, actionCodeSettings)
+        .then(() => {
+          window.localStorage.setItem("emailForSignIn", this.email);
+          this.activeScreen = screens.sent;
+        })
+        .catch((error) => {
+          alert(`Error signing in (${error})`);
+        });
+    },
+  },
 };
 </script>
 
