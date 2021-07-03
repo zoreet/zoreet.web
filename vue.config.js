605e45ab2e7731cdfd02c38819ef56c57596afbb
zoreet <zoreetdotcom@gmail.com>
2021-07-03 10:09:34 +0200

    Since I couldn't get vite to work on building for production I'm removeing it until I move to vue3

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,7 @@ module.exports = {
       .use("vue-loader")
       .loader("vue-loader")
       .tap((options) => {
-        // modify the options...
-        // options.compilerOptions.whitespace = "preserve";
+        options.compilerOptions.whitespace = "preserve";
         return options;
       });
   },
